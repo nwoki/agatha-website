d8a1fa9e2bdc60e535d24e36a63fd6a704777739
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2013-07-28 12:17:33 +0200

    added 'add gameserver' form for user

--- functions/usercpfunctions.php
+++ functions/usercpfunctions.php
@@ -9,6 +9,29 @@
     require_once 'PHP-on-Couch/lib/couchClient.php';
     require_once 'PHP-on-Couch/lib/couchDocument.php';
 
+    function addGameserver($ip, $name) {
+        if (!isset($ip) || !isset($name)) {
+            // alert user with simple js alert function
+        } else {
+            // add to database
+            $link = connectDb("gameservers");
+            $newDoc = new stdClass();
+            $newDoc->ip = $ip;
+            $newDoc->name = $name;
+            $newDoc->admin = $_SESSION['username'];
+
+            try {
+                $response = $link->storeDoc($newDoc);
+            } catch (Exception $e) {
+                $error = explode('(', $e->getMessage());
+                echo "<script language=javascript>alert(\"$error[0]\")</script>";
+            }
+        }
+
+        // post-redirect-get to avoid duplicate calls
+//         header("Location: " . $_SERVER['REQUEST_URI']);
+    }
+
     function deleteGameserver($gameserverId) {
         $link = connectDb("gameservers");
         $doc = $link->getDoc($gameserverId);
@@ -17,6 +40,7 @@
         // post-redirect-get to avoid duplicate calls
         header("Location: " . $_SERVER['REQUEST_URI']);
     }
+
 
     function listUserGameservers($username) {
         $link = connectDb("gameservers");
@@ -45,17 +69,40 @@
                 <td>".$doc->get("name")."</td>
                 <td>".$doc->get("admin")."</td>
                 <td>
-                <div class=button>
-                    <form action=# method=post>
-                        <input type=hidden name=delete_gameserver value=$row->id>
-                        <input type=submit value=Delete>
-                    </form>
-                </div>
+                    <div class=button>
+                        <form action=# method=post>
+                            <input type=hidden name=delete_gameserver value=$row->id>
+                            <input type=submit value=Delete>
+                        </form>
+                    </div>
                 </td>
                 </tr>";
         }
 
         // close the table
         echo "</tbody></table></div>";
+
+        // add gameserver button
+        echo "
+            <div class=\"button\">
+                <form action=\"#\" method=\"post\">
+                    <input type=\"hidden\" name=\"show_add_gameserver_form\" value=\"true\">
+                    <input type=\"submit\" value=\"Add Gameserver\">
+                </form>
+            </div>";
+    }
+
+
+    function showAddGamerserverForm() {
+        echo "
+            <div class=form>
+                <h1>New Gameserver</h1>
+                <form action=# method=post name=add_gameserver_form onsubmit=validate()>
+                    <input type=hidden name=add_gameserver>
+                    <input type=text name=new_gameserver_ip placeholder=\"gameserver ip address\">
+                    <input type=text name=new_gameserver_name placeholder=\"gamserver name\">
+                    <input type=submit value=create>
+                </form>
+            </div>";
     }
 ?>
--- pages/usercp_page.php
+++ pages/usercp_page.php
@@ -4,20 +4,21 @@
         header('Location: 404.php');
     }
 
+    // add gameserver
+    if (isset($_POST['add_gameserver'])) {
+        addGameserver($_POST['new_gameserver_ip'], $_POST['new_gameserver_name']);
+    }
+
     // delete gameserver
     if (isset($_POST['delete_gameserver'])) {
-        echo "deleting gameserver ".$_POST['delete_gameserver'];
         deleteGameserver($_POST['delete_gameserver']);
     }
 
-    // Show users currently registered gameservers
-    listUserGameservers($_SESSION['username']);
+    // add gameserver
+    if (isset($_POST['show_add_gameserver_form'])) {
+        showAddGamerserverForm();
+    } else {
+        // Show users currently registered gameservers
+        listUserGameservers($_SESSION['username']);
+    }
 ?>
-
-    <!-- add gameserver button -->
-    <div class="button">
-        <form action="#" method="post">
-            <input type="hidden" name="add_gameserver" value="true">
-            <input type="submit" value="Add Gameserver">
-        </form>
-    </div>
